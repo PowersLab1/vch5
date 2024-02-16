41544fe236e834f1297d04513691b014f7294406
Max Greenwald <powerslab3@gmail.com>
2024-02-16 14:41:48 -0500

    Updates

---
+++ precache-manifest.962ced0879718ad5935d45e104cc606f.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "c328d24098a4c4a67a04",
+    "url": "/vch5/static/css/main.bb0bc7a4.chunk.css"
+  },
+  {
+    "revision": "c328d24098a4c4a67a04",
+    "url": "/vch5/static/js/main.00b0a104.chunk.js"
+  },
+  {
+    "revision": "1a0e648e3429048d33b1",
+    "url": "/vch5/static/js/runtime~main.46d04184.js"
+  },
+  {
+    "revision": "c5ff417aaa90be933bf1",
+    "url": "/vch5/static/js/2.2e0418af.chunk.js"
+  },
+  {
+    "revision": "6956dbda0015b26b8daac462ec922ef5",
+    "url": "/vch5/static/media/psych_logo.6956dbda.jpg"
+  },
+  {
+    "revision": "b070f1811207348f54b4988fc14bc950",
+    "url": "/vch5/static/media/rating_keydown_1.b070f181.png"
+  },
+  {
+    "revision": "14052abb8ad3bf096d71788ba2e2ace2",
+    "url": "/vch5/static/media/rating_keydown_2.14052abb.png"
+  },
+  {
+    "revision": "bc40f6358057323efebef6ea2db6cb59",
+    "url": "/vch5/static/media/rating_keydown_3.bc40f635.png"
+  },
+  {
+    "revision": "852779dc3fb4af1fd92cb2611fe82806",
+    "url": "/vch5/static/media/rating_keydown_4.852779dc.png"
+  },
+  {
+    "revision": "ae6e6c1fe3fef7ab5aa19a93442d23d3",
+    "url": "/vch5/static/media/rating_keydown_5.ae6e6c1f.png"
+  },
+  {
+    "revision": "81dd3c420ae8f93737c2699737bdf01d",
+    "url": "/vch5/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/vch_short_psychedelic_5/precache-manifest.d2e8c4c3052bfe7ca3edeaebf462cf46.js"
+  "/vch5/precache-manifest.962ced0879718ad5935d45e104cc606f.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/vch_short_psychedelic_5/index.html", {
+workbox.routing.registerNavigationRoute("/vch5/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
